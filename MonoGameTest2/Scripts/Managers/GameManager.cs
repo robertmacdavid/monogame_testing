0b8ec2024d0cecb4dc5bfcae37a388e4e8d56979
James Baker <bakerjm702@gmail.com>
2018-09-01 00:10:27 -0700

    Added debug info.

--- MonoGameTest2/Scripts/Managers/GameManager.cs
+++ MonoGameTest2/Scripts/Managers/GameManager.cs
@@ -14,9 +14,12 @@ namespace MonoGameTest2.Managers
         public float DeltaTime;
         public Player Player;
 
+        private SpriteFont _font;
+
         public void LoadContent(ContentManager contentManager)
         {
             var avatar = contentManager.Load<Texture2D>("images/SmileyWalk");
+            _font = contentManager.Load<SpriteFont>("default_font");
 
             Player = new Player(avatar, new Vector2(0, 0));
             Player.LoadContent(contentManager);
@@ -33,6 +36,10 @@ namespace MonoGameTest2.Managers
         public void Draw(SpriteBatch spriteBatch)
         {
             Player.Draw(spriteBatch);
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(_font, string.Format("Debug Info\nDelta Time: {0}\nPlayer Position: {1}", DeltaTime, Player.Position), new Vector2(0, 0), Color.White);
+            spriteBatch.End();
         }
     }
 }
